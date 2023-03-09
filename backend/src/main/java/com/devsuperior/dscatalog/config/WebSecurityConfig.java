e25c68a14972b4327827c6378dbe9abd840ed8d2
matheusktt <matheus_0119@outlook.com>
2023-03-09 14:41:56 -0300

    import package

--- backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/WebSecurityConfig.java
@@ -1,3 +1,5 @@
+package com.devsuperior.dscatalog.config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
