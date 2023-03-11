e98e61d43854cfd099cfaf7ee79716f6c6cc7742
matheusktt <matheus_0119@outlook.com>
2023-03-11 17:37:29 -0300

    h2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,15 +10,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment environment;
+
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    // Endpoint publico para login
-    private static final String[] PUBLIC = {"/oauth/token"};
+    // Endpoint publico para login e h2-console
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
     // Endpoints liberados para operador e admin
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
@@ -34,6 +40,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     // Configuração das rotas
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        // Libera o h2-console no perfil de test, desabilitando os frames do h2
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
